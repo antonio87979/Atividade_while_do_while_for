e7fc61cdf20743d976a8e74ac8b7a194799b622f
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-26 15:11:10 -0300

    Update Program.cs ex02

--- ex02/Program.cs
+++ ex02/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Um usuário quer calcular suas despesas mensais. O exercício consiste em solicitar 
+//ao usuário que insira suas despesas até que ele digite um valor negativo, 
+//sinalizando o fim da entrada. O aluno deve somar as despesas e apresentar o total, 
+//utilizando while, do while e for.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+
+		double despesas = 0;
+    double despesa;
+		// Usando while 
+/*Console.WriteLine("Digite suas despesas (valor negativo para sair):");
+
+while (true)
+{
+ despesa = Convert.ToDouble(Console.ReadLine());
+ if (despesa < 0) break;
+ despesas += despesa;
+}
+Console.WriteLine($"Total de despesas: {despesas}");
+*/
+   // Usando do while 
+
+/*do
+{
+ Console.WriteLine("Digite suas despesas (valor negativo para sair):"); 
+ despesa = Convert.ToDouble(Console.ReadLine());
+ if (despesa >= 0) despesas += despesa;
+}
+while (despesa >= 0);
+
+Console.WriteLine($"Total de despesas: {despesas}");
+*/
+   // Usando for 
+
+Console.WriteLine("Digite suas despesas (valor negativo para sair):"); //pedindo os valores
+for(;;) 
+{
+ despesa = Convert.ToDouble(Console.ReadLine());
+ if (despesa <= 0) break;
+ despesas += despesa;
+}
+Console.WriteLine($"Total de despesas: {despesas}");
+	}
+}
