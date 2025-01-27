11efd0b5f569c1c6385de78768a9ae903ecd94e7
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-27 00:09:02 -0300

    Update Program.cs ex04

--- ex04/Program.cs
+++ ex04/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Um grupo de amigos deseja calcular a idade média deles. Para isso, o aluno deve 
+//pedir que o usuário insira as idades e calcule a média até que o usuário digite 0. A 
+//solução deve ser implementada usando while, do while e for.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double somaIdades = 0;
+		int contador = 0;
+        int idade;
+		
+//Usando While
+/*
+Console.WriteLine("Digite as idades (0 para sair):");
+while (true)
+{
+ idade = Convert.ToInt32(Console.ReadLine());
+ if (idade== 0) break;
+ somaIdades += idade;
+ contador++;
+}
+		double mediaIdade = contador > 0? somaIdades / contador: 0; 
+		Console.WriteLine($"Média das idades: {mediaIdade}");
+*/
+// Usando do while 
+/*
+do
+{
+ Console.WriteLine("Digite as idades (0 para sair):");
+ idade  = Convert.ToInt32(Console.ReadLine());
+ if (idade != 0) 
+   {
+    somaIdades += idade;
+    contador++;
+   }
+ } while (idade != 0);
+        double mediaIdade = contador > 0? somaIdades / contador: 0; 
+		Console.WriteLine($"Média das idades: {mediaIdade}");
+*/
+// Usando for
+
+for (;;)
+{
+ Console.WriteLine("Digite as idades (0 para sair):"); 
+ idade = Convert.ToInt32(Console.ReadLine());
+ if (idade == 0) break;
+ somaIdades += idade;
+ contador++;
+} 
+  double mediaIdade = contador > 0? somaIdades / contador: 0; 
+  Console.WriteLine($"Média das idades: {mediaIdade}");
+
+	}
+}
