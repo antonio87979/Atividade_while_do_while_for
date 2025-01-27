3d604795e7b7d7e170f21551c144dfcad889eb23
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-27 16:01:10 -0300

    Update Program.cs ex07

--- ex07/Program.cs
+++ ex07/Program.cs
@@ -1,3 +1,47 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Um ciclista deseja saber a distância total que percorreu. Para isso, o aluno deve 
+//solicitar que o usuário insira as distâncias percorridas em cada dia até que um valor 
+//negativo seja digitado. A soma das distâncias deve ser calculada e exibida 
+//utilizando as três estruturas de repetição.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+    double distanciaTotal = 0;
+    double distancia;
+//usando while
+		
+Console.WriteLine("Digite as distâncias percorridas (valor negativo para sair):");
+while (true)
+{
+ distancia = Convert.ToDouble(Console.ReadLine());
+ if (distancia < 0) break;
+ distanciaTotal += distancia;
+}
+Console.WriteLine($"Distância total percorrida: {distanciaTotal} km");
+
+// Usando do while
+/*
+do
+{
+ Console.WriteLine("Digite as distâncias percorridas (valor negativo para sair):");
+ distancia = Convert.ToDouble(Console.ReadLine());
+ if (distancia >= 0) distanciaTotal += distancia;
+} while (distancia >= 0);
+Console.WriteLine($"Distância total percorrida: {distanciaTotal} km");
+
+// Usando for distanciaTotal = 0;
+
+for (;;)
+{
+ Console.WriteLine("Digite as distâncias percorridas (valor negativo para sair):");
+ distancia = Convert.ToDouble(Console.ReadLine());
+ if (distancia < 0) break;
+ distanciaTotal += distancia;
+}
+ Console.WriteLine($"Distância total percorrida: {distanciaTotal} km");
+ */
+	}
+}
