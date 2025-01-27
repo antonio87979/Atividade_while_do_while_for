fe76ad3b5d4c0b4014a212c1cdd0ab98264ed1a8
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-27 16:03:12 -0300

    Update Program.cs ex08

--- ex08/Program.cs
+++ ex08/Program.cs
@@ -1,3 +1,47 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Um atendente de suporte técnico deseja contar quantas chamadas atendeu em 
+//um dia. O aluno deverá pedir ao usuário que insira o tempo de duração de cada 
+//chamada em minutos até que ele digite 0. O programa deve calcular o total de
+//minutos atendidos, utilizando while, do while e for.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+    int totalchamadas = 0;
+    int duracao;
+//usando while		
+
+Console.WriteLine("Digite a duração das chamadas em minutos (0 para sair):"); 
+while (true)
+{
+ duracao = Convert.ToInt32(Console.ReadLine());
+ if (duracao == 0) break;
+ totalchamadas += duracao;
+}
+Console.WriteLine($"Total de minutos atendidos: {totalchamadas} minutos");
+
+// Usando do while
+/*
+do
+{
+ Console.WriteLine("Digite a duração das chamadas em minutos (0 para sair):");
+ duracao = Convert.ToInt32(Console.ReadLine());
+ if (duracao != 0) totalchamadas += duracao;
+} while (duracao != 0);
+Console.WriteLine($"Total de minutos atendidos: {totalchamadas} minutos");
+
+// Usando for
+
+for (;;)
+{
+ Console.WriteLine("Digite a duração das chamadas em minutos (0 para sair):");
+ duracao = Convert.ToInt32(Console.ReadLine());
+ if (duracao == 0) break;
+ totalchamadas += duracao;
+}
+Console.WriteLine($"Total de minutos atendidos: {totalchamadas} minutos");
+*/
+ }
+}
