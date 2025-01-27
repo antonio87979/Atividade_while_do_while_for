140e21b277b47f9dd2601f34ff6b40fad69a1175
Antônio Romeu <antonio.rmsneto@senacsp.edu.br>
2025-01-27 00:06:51 -0300

    Update Program.cs ex03

--- ex03/Program.cs
+++ ex03/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Uma escola deseja saber quantos alunos foram aprovados em uma disciplina. O 
+//aluno deverá solicitar que o usuário insira as notas dos alunos (de 0 a 10) e, ao 
+//digitar -1, encerrará a entrada. Em seguida, deve contar quantos alunos tiveram 
+//nota maior ou igual a 6, utilizando as três estruturas de repetição.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+int alunosAprovados = 0;
+double nota;
+		
+//Usando While
+		
+Console.WriteLine("Digite as notas dos alunos (digite -1 para sair):");
+
+while (true)
+{
+ nota = Convert.ToDouble(Console.ReadLine());
+ if (nota == -1) break;
+ if (nota >= 6) alunosAprovados++;
+}
+ Console.WriteLine($"Total de alunos aprovados: {alunosAprovados}");
+
+// Usando do while 
+/*
+do
+{
+ Console.WriteLine("Digite as notas dos alunos (digite -1 para sair):");
+ nota  = Convert.ToDouble(Console.ReadLine());
+ if (nota >= 6 && nota !=-1) alunosAprovados++;
+}while (nota != -1);
+
+Console.WriteLine($"Total de alunos aprovados: {alunosAprovados}");
+*/
+// usando for
+/*
+for (;;)
+{
+ Console.WriteLine("Digite as notas dos alunos (digite -1 para sair):");
+ nota = Convert.ToDouble(Console.ReadLine());
+ if (nota == -1) break;
+ if (nota >= 6) alunosAprovados++;
+}
+Console.WriteLine($"Total de alunos aprovados: {alunosAprovados}");
+*/		
+	}
+}
